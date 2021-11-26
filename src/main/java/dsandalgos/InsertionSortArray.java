4b29433532601fd16d31cb2415d1aa4f2462bac6
mboyce <meejahnsnutshell@users.noreply.github.com>
2021-11-25 17:56:04 -0700

    docs

--- src/main/java/dsandalgos/InsertionSortArray.java
+++ src/main/java/dsandalgos/InsertionSortArray.java
@@ -1,5 +1,24 @@
 package dsandalgos;
 
+/**
+ * <p>
+ * Sorts array in ascending order using insertion sort method. Splits array into
+ * two sides - sorted on the left and unsorted on the right. We begin the outer
+ * loop at index = 1 and consider the 0 index to be the sorted array. As the
+ * outer loop increments, an inner loop decrements (moves to the left). The
+ * outer loop element is compared to each element to the left. When a value is
+ * found less than it, the outer loop element is inserted to the inner index +
+ * 1.
+ * <p>
+ * <p>
+ * Time complexity <br>
+ * Worst case scenario is O(n^2) when the list is sorted in descending order.
+ * Best case scenario is O(n) when the list is already sorted in ascending
+ * order.
+ * <p>
+ * 
+ * @author Meghan Boyce
+ */
 public class InsertionSortArray {
 
 	public static void main(String[] args) {
